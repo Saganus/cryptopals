40c0d0def5640dfb4935ee60a153d411ce9085e7
Akram Shehadi <akram.shehadi@gmail.com>
2017-04-05 22:51:15 -0500

    Added expected value

--- set1/level2.py
+++ set1/level2.py
@@ -17,8 +17,12 @@ def xor_hex(input1, input2):
 
 input1 = '1c0111001f010100061a024b53535009181c'
 input2 = '686974207468652062756c6c277320657965'
+expected = b'746865206b696420646f6e277420706c6179'
+result = xor_hex(input1, input2)
 
-print('XOR result: ', xor_hex(input1, input2))
+print('XOR result: ', result)
 
+if(expected == result):
+	print('SUCCESS!')
 
 
